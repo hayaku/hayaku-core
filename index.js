47a0d3b71d6a375e7110aa38c57c025c66f8c644
Roman Komarov <kizmarh@ya.ru>
2017-12-21 00:46:22 +0100

    Added eslint & editorconfig

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const rankAndSort = require('./lib/rank-and-sort.js');
 const Hayaku = {
   expandDeclaration: abbr => rankAndSort(abbrToResults(abbr)),
   expandProp: abbr => rankAndSort(abbrToResults(abbr + ':'))
-}
+};
 
 module.exports = Hayaku;
